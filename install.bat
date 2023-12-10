9912914a21df69f16d96fed37df7ff8f2bd3c9c4
Александр <54349566+maj0roff@users.noreply.github.com>
2023-12-10 22:09:38 +0500

    Delete install.bat

--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001
-cls
-echo Установка необходимых пакетов
-echo Убедитесь, что у вас Python 3.11.5
-python -m pip install -r requirements.txt
-echo Скрипт установки завершён
-pause
