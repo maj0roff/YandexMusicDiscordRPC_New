79ddc71f6025bfab9c1ce13bcb8001a98742cb12
Александр <54349566+maj0roff@users.noreply.github.com>
2024-01-30 19:36:14 +0500

    Add files via upload

--- apps/userinterface/main.py
+++ apps/userinterface/main.py
@@ -1,7 +1,7 @@
 import webbrowser
 import wx
 import wx.adv
-
+import re
 import os
 import psutil
 import requests
@@ -137,9 +137,30 @@ class App(wx.App):
         return True
 
 
-def run_ui():
+def update(version):
+    r = requests.get("https://api.github.com/repos/maj0roff/YandexMusicDiscordRPC_New/releases/latest")
+    latest_version = r.json()["tag_name"]
+    update_log = re.sub(r'!\[image\]\(.*?\)', "", r.json()["body"])
+    if latest_version != version:
+        resp = wx.MessageBox(f'Обнаружена новая версия программы.'
+                             f'\n\nСписок изменений:{update_log}'
+                             f'\n\nХотите обновиться?',
+                             'Обновление',
+                             wx.OK | wx.CANCEL | wx.ICON_INFORMATION)
+        if resp == wx.OK:
+            print(os.getcwd())
+            os.system(f"{os.getcwd()}\\updater.exe")
+            current_system_pid = os.getpid()
+
+            ThisSystem = psutil.Process(current_system_pid)
+            ThisSystem.terminate()
+        else:
+            return
+
+def run_ui(version):
     app = App(False)
     app.MainLoop()
+    update(version)
     current_system_pid = os.getpid()
 
     ThisSystem = psutil.Process(current_system_pid)
